2dd65b2ba6785d87b9d3e70326c33ab598fc7855
Priyanshu Kumar Sinha <priyanshukusinha.com>
2024-07-21 15:07:44 +0530

    resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,9 +43,9 @@ function About() {
               alt=""
             />
           </div>
-          <div className="mt-7 flex items-center justify-center p-2 mx-1 font-bold bg-slate-800 text-red-600 rounded-md">
+          <div className="mt-7 flex items-center justify-center mx-1 font-bold bg-slate-800 text-red-600 rounded-md">
             <a
-              className="flex items-center gap-2"
+              className="flex items-center gap-2  px-14 py-2 rounded-md"
               href="https://drive.google.com/file/d/13bjMJgK1FJ27og-eEeh8Szpt2ZVtlrmk/view?usp=drive_link"
             >
               View Resume <FaFilePdf />
