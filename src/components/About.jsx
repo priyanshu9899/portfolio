2a353365fbc5ee72d22d25678784bb8c780cdd91
Priyanshu Kumar Sinha <priyanshukusinha.com>
2024-07-30 17:44:57 +0530

    suspense fallback added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,66 +1,12 @@
-import { FaFilePdf } from "react-icons/fa6";
-import Navbar from "./Navbar";
-import Experience from "./Experience";
-import Education from "./Education";
-import Footer from "./Footer";
+import React, { Suspense } from "react";
+import LoadingPage from "./LoadingPage";
+const AboutContent = React.lazy(() => import("./AboutContent"))
 
 function About() {
   return (
-    <div className="h-[100vh] w-full relative bg-zinc-900">
-      <Navbar />
-      <div className="w-full flex gap-12 gap-x-40 justify-center items-center flex-wrap px-[3vw] pt-20 ">
-        <div className="h-[450px] max-xl:h-[650px] w-[850px] max-xl:order-2 ">
-          <div className="mb-10 heading-area text-4xl max-xl:text-[7vw] tracking-tighter">
-            Hey, I&apos;m <span className="text-cyan-400">Priyanshu</span> Kumar
-            Sinha
-          </div>
-          <p className="tracking-tighter text-zinc-400 text-[16px] max-xl:text-[4vw] max-xl:leading-relaxed">
-            I&apos;m a college student from Jamshedpur currently residing in
-            Chennai, passionate about becoming a full stack web developer. With
-            a focus on creating intuitive applications, I leverage technologies
-            like React, Node.js, and MongoDB to build scalable solutions. My
-            enthusiasm for problem-solving, especially in Data Structures and
-            Algorithms (DSA), drives me to optimize solutions for efficiency and
-            performance. Beyond coding, I actively pursue opportunities to
-            expand my skills, ensuring a well-rounded approach to software
-            development.
-          </p>
-          <div className="tracking-tighter xl:mt-20 mt-8 rounded-[20px]">
-            <p className="px-4 bg-cyan-700 rounded-[20px] max-xl:text-[4vw] text-zinc-200 text-[17px] p-3">
-              If you ever spot me in the wild, don&apos;t hesitate to say hello!
-              Let&apos;s grab a drink and geek-out over the latest advancements
-              in full-stack development or discuss the new trending topic in 0-1
-              world. ✨
-            </p>
-          </div>
-        </div>
-        <div className="h-[450px] max-xl:h-[500px] w-[300px] flex-col flex max-xl:order-1 ">
-          <div className="relative group">
-            <div className="absolute -inset-1 bg-cyan-400 rounded-[30px] max-xl:opacity-50 opacity-65 blur-xl transition duration-1000 xl:group-hover:opacity-100 group-hover:duration-300"></div>
-            <img
-              className="h-[450px] flex items-center justify-center rounded-[30px] relative"
-              src="/profile.jpg"
-              alt=""
-            />
-          </div>
-          <div className="mt-7 flex items-center justify-center mx-1 font-bold bg-slate-800 text-red-600 rounded-md">
-            <a
-              className="flex items-center gap-2  px-14 py-2 rounded-md"
-              href="https://drive.google.com/file/d/13bjMJgK1FJ27og-eEeh8Szpt2ZVtlrmk/view?usp=drive_link"
-            >
-              View Resume <FaFilePdf />
-            </a>
-          </div>
-        </div>
-        <div className="w-full max-xl:order-3">
-          <Experience />
-        </div>
-        <div className="w-full max-xl:order-4">
-          <Education />
-        </div>
-      </div>
-      <Footer />
-    </div>
+      <Suspense fallback={<LoadingPage />}>
+        <AboutContent />
+      </Suspense>
   );
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,91 +1,13 @@
-import { Link } from 'react-router-dom'
-import Footer from './Footer'
-import Navbar from './Navbar'
+import React, { Suspense } from "react";
+import LoadingPage from "./LoadingPage";
+const ProjectsContent = React.lazy(() => import("./ProjectsContent"))
 
 function Projects() {
-
-  const ProjectList = [
-    {
-      link: "/dyslexify",
-      heading:"Dyslexify AI",
-      description:"A speech summarising extension made with multiple AI models. It can summarise whole webpage in just 4 lines which saves your time and can be helpful for those who are suffering from dyslexia.",
-      imgSrc:"/dyslexify.png"
-    },
-    {
-      link:"/new",
-      heading:"Coming Soon...",
-      description:"Currently working on this project, stay tuned for this one !!",
-      imgSrc:"/loading.jpg"
-    }
-  ]
-
-  const PracticeProjectList = [
-    {
-      heading:"Music Player",
-      description:"Basically a spotfy clone but having extra features for free such as download option , skip and fast-forward songs.",
-      imgSrc:"/music.png"
-    },
-    {
-      heading:"Note Me",
-      description:"A note taking app made with ejs and express.js [backend], You can do all CRUD(create , read , update and delete) operations in this . ",
-      imgSrc:"/note.avif"
-    },
-    {
-      heading:"Password Generator",
-      description:"This was made by me while learning states and hooks in React , you can use this to generate password of any length with the characters of your choice.",
-      imgSrc:"/password.png"
-    }
-  ]
-
-
   return (
-    <div className=' w-full h-full relative '>
-        <Navbar/>
-        <div className=' px-12 pt-16 max-xl:px-[6vw] '>
-          <h1 className='heading-area text-4xl mb-5 text-cyan-400'>Projects</h1>
-          <p className='tracking-tighter leading-tight text-base text-[18px] xl:w-3/4 text-zinc-400'>I&apos;ve worked on these few projects over the years.
-             All of them are open-source, so if you see something that piques your interest, check out the code and contribute 
-             if you have ideas on how it can be improved.</p>
-        </div>
-        <div className='w-full max-xl:px-[6vw] py-12 px-12 flex max-xl:items-center max-xl:justify-center gap-12 flex-wrap'>
-          {ProjectList.map((item, index) => (
-            <Link key={index} to={item.link}>
-              <div className='w-[400px] max-xl:w-[360px] px-1 h-full rounded-3xl bg-zinc-800 flex justify-center hover:border hover:border-cyan-400 '>
-                <div className='w-[80px] rounded-l-3xl mx-1 flex items-center justify-center'>
-                  <img className='w-[60px] h-[60px]' src={item.imgSrc} alt="" />
-                </div>
-                <div className='w-[360px] rounded-r-3xl'>
-                  <div className='rounded-tr-3xl p-2 text-3xl tracking-tight heading-area'>{item.heading}</div>
-                  <div className='rounded-br-3xl p-2 tracking-tighter text-sm text-zinc-300/70 leading-tight'>{item.description}</div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-
-        <div className=' px-12 pt-5 max-xl:px-[6vw]'>
-          <h1 className='heading-area text-4xl mb-5 text-cyan-400'> Practice Projects</h1>
-          <p className='tracking-tighter leading-tight text-base text-[18px] xl:w-3/4 text-zinc-400'>
-          These are some of the practice projects I have completed while learning fundamental and essential concepts. 
-          Each of these projects is close to my heart as they represent significant steps in my learning journey. 
-          </p>
-        </div>
-        <div className=' w-full max-xl:px-[6vw] py-12  px-12 flex max-xl:items-center max-xl:justify-center gap-12 flex-wrap'>
-          {PracticeProjectList.map((item, index) => (
-              <div key={index} className='w-[400px] max-xl:w-[360px] min-h-fit rounded-3xl bg-zinc-800 flex justify-center'>
-                <div className='  w-[80px] rounded-l-3xl mx-1 flex items-center justify-center'>
-                  <img className='w-[60px] h-[60px]' src={item.imgSrc} alt="" />
-                </div>
-                <div className='w-[360px] rounded-r-3xl '>
-                  <div  className=' rounded-tr-3xl p-2  text-3xl tracking-tight heading-area'>{item.heading}</div>
-                    <div  className=' rounded-br-3xl p-2 tracking-tighter text-sm text-zinc-300/70 leading-tight'>{item.description}</div>
-                  </div>
-              </div>
-            ))}
-        </div>
-        <Footer />
-    </div>
-  )
+      <Suspense fallback={<LoadingPage />}>
+        <ProjectsContent />
+      </Suspense>
+  );
 }
 
 export default Projects;
